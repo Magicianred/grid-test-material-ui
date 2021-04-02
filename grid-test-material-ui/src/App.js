b5e48e63d511e7114594de8efd8ed85b9e60b995
Fojan Babaali <fozhanmail@gmail.com>
2021-04-01 18:02:54 -0700

    update

--- grid-test-material-ui/src/App.js
+++ grid-test-material-ui/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+// ---------------------------------
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import AppBar from "@material-ui/core/AppBar";
@@ -9,8 +10,13 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import MenuIcon from "@material-ui/icons/Menu";
 import Button from "@material-ui/core/Button";
+import Divider from '@material-ui/core/Divider';
+
+
+// ---------------------------------------------
 
 import "./App.css";
+const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -27,8 +33,16 @@ const useStyles = makeStyles((theme) => ({
   },
   title: {
     flexGrow: 1,
-    textAlign:"left"
+    textAlign: "left",
   },
+  sidBarItems:{
+    display:"flex",
+    flexGrow:2
+  },
+  space:{
+    marginTop:"60px"
+  }
+
 }));
 
 function App() {
@@ -38,10 +52,24 @@ function App() {
     <div className="App">
       <CssBaseline />
       <Grid container spacing={0}>
-        <Grid item md={2} sm={6} xs={2}>
-          <Paper className={classes.paper}>sideBar</Paper>
+        <Grid item md={2} sm={3} xs={2}>
+          <Paper className={classes.paper}>
+            <div className={classes.space}></div>
+            <Divider/>
+            <div className={classes.sidBarItems}>Status</div>
+            <div className={classes.sidBarItems}>Dashboard</div>
+            <div className={classes.sidBarItems}>Dashboard</div>
+            <div className={classes.sidBarItems}>Dashboard</div>
+            <div className={classes.sidBarItems}>Dashboard</div>
+            <div className={classes.sidBarItems}>Dashboard</div>
+            <div className={classes.sidBarItems}>Dashboard</div>
+            <div className={classes.sidBarItems}>Dashboard</div>
+            <div className={classes.sidBarItems}>Dashboard</div>
+            <Divider/>
+
+          </Paper>
         </Grid>
-        <Grid item md={10} sm={6} xs={10}>
+        <Grid item md={10} sm={9} xs={10}>
           <AppBar position="static">
             <Toolbar>
               <IconButton
@@ -53,7 +81,7 @@ function App() {
                 <MenuIcon />
               </IconButton>
               <Typography variant="h6" className={classes.title}>
-                News
+                My Application
               </Typography>
               <Button color="inherit">Login</Button>
             </Toolbar>
