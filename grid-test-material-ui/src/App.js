b950a56b8a324821cb40ebfb53cdf9745e6de0f9
Fojan Babaali <fozhanmail@gmail.com>
2021-04-01 17:37:09 -0700

    update

---
+++ grid-test-material-ui/src/App.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import AppBar from "@material-ui/core/AppBar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { makeStyles } from "@material-ui/core/styles";
+import IconButton from "@material-ui/core/IconButton";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import MenuIcon from "@material-ui/icons/Menu";
+import Button from "@material-ui/core/Button";
+
+import "./App.css";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+    minHeight: "100vh",
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    textAlign:"left"
+  },
+}));
+
+function App() {
+  const classes = useStyles();
+
+  return (
+    <div className="App">
+      <CssBaseline />
+      <Grid container spacing={0}>
+        <Grid item md={2} sm={6} xs={2}>
+          <Paper className={classes.paper}>sideBar</Paper>
+        </Grid>
+        <Grid item md={10} sm={6} xs={10}>
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton
+                edge="start"
+                className={classes.menuButton}
+                color="inherit"
+                aria-label="menu"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" className={classes.title}>
+                News
+              </Typography>
+              <Button color="inherit">Login</Button>
+            </Toolbar>
+          </AppBar>
+          <Paper className={classes.paper}>cards and header</Paper>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
+
+export default App;
